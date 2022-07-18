5ab7f9dad1f4e5f62c791629e8896426118b85f3
Alex Grinevsky <alex.grinevsky@gmail.com>
2022-07-19 00:58:57 +0200

    added rendertree and comments

--- src/components/Profile/Posts/Posts.tsx
+++ src/components/Profile/Posts/Posts.tsx
@@ -7,27 +7,27 @@ type PostsPropsType = {
     posts: Array<PostsType>
     addPost: ( postText: string ) => void
     newPostMessage: string
-    changeNewPostMessage: (newText: string) => void
+    changeNewPostMessage: ( newText: string ) => void
 }
 
-export const Posts = (props: PostsPropsType) => {
+export const Posts = ( props: PostsPropsType ) => {
 
     let postsElements = props.posts.map(( p ) => <Post key={p.id} message={p.message} likesCount={p.likesCount}/>);
 
-    let newPostElement = React.createRef<HTMLTextAreaElement>();
-
-    let addPost = () => {
-        if (newPostElement.current){
-        props.addPost(newPostElement.current.value);
-        newPostElement.current.value = '';
-        }
+    const addPost = () => {
+        props.addPost(props.newPostMessage);
+        props.changeNewPostMessage('')
     }
+
+    const newPostChangeHandler = ( e: ChangeEvent<HTMLTextAreaElement> ) => {
+        props.changeNewPostMessage(e.currentTarget.value);
+    };
 
     return (
         <div>
             <h3>My Posts</h3>
             <div>
-                <textarea ref={newPostElement} value={props.newPostMessage}></textarea>
+                <textarea onChange={newPostChangeHandler} value={props.newPostMessage}/>
             </div>
             <button onClick={addPost}>add post</button>
             <div className={classes.posts}>
@@ -36,29 +36,3 @@ export const Posts = (props: PostsPropsType) => {
         </div>
     )
 }
-
-// export const Posts = ( props: PostsPropsType ) => {
-//
-//     let postsElements = props.posts.map(( p ) => <Post key={p.id} message={p.message} likesCount={p.likesCount}/>);
-//
-//     const addPost = () => {
-//         props.addPost(props.newPostMessage);
-//     }
-//
-//     const newPostChangeHandler = ( e: ChangeEvent<HTMLTextAreaElement> ) => {
-//         props.changeNewPostMessage(e.currentTarget.value);
-//     };
-//
-//     return (
-//         <div>
-//             <h3>My Posts</h3>
-//             <div>
-//                 <textarea value={props.newPostMessage} onChange={newPostChangeHandler}></textarea>
-//             </div>
-//             <button onClick={addPost}>add post</button>
-//             <div className={classes.posts}>
-//                 {postsElements}
-//             </div>
-//         </div>
-//     )
-// }
